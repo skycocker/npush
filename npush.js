bf6c58ba54b5c54a2fe0ca2ef1b4ce14473763a4
Michał Siwek <mike21@aol.pl>
2013-03-03 02:25:53 +0100

    Enhance code parts separation

--- npush.js
+++ npush.js
@@ -33,6 +33,7 @@ io.configure(function() {
   io.set("transports", ["xhr-polling"]);
   io.set("polling duration", 10);
 });
+//
 
 io.sockets.on('connection', function(socket) {
   socket.emit('connection', { status: 'connected! :)' } );
